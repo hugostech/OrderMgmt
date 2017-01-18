824d3502b603635d5817f06fddc4a47fb915a3dd
hugo <hugowangchn@gmail.com>
2017-01-18 22:18:02 +1300

    create order items

---
+++ database/migrations/2017_01_18_090537_create_order_items_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrderItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('order_items', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->integer('product_id');
+            $table->integer('quantity');
+            $table->string('name');
+            $table->float('price');
+            $table->float('cost');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order_items');
+    }
+}
