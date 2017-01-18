912fa3d3c0f95eb0d6645315a78553ef06efa65d
hugo <hugowangchn@gmail.com>
2017-01-18 22:16:54 +1300

    add order migration attributes

--- database/migrations/2017_01_18_035439_create_orders_table.php
+++ database/migrations/2017_01_18_035439_create_orders_table.php
@@ -16,6 +16,18 @@ class CreateOrdersTable extends Migration
         Schema::create('orders', function (Blueprint $table) {
             $table->increments('id');
             $table->timestamps();
+            $table->date('order_create_at')->nullable();
+            $table->float('total');
+            $table->float('feight')->nullable();
+            $table->enum('shipping_method',[0,1])->default(0);
+            $table->string('shipping_name')->nullable();
+            $table->string('shipping_company')->nullable();
+            $table->string('shipping_phone')->nullable();
+            $table->string('shipping_postcode')->nullable();
+            $table->text('shipping_address')->nullable();
+            $table->string('payment_method');
+            $table->string('currency_code');
+            $table->enum('payment_status',[0,1])->default(0);
         });
     }
 
